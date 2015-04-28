023b94cac193fcbd00a04c15be1ed977645bf433
Davis Mosenkovs <davikovs@gmail.com>
2015-04-28 08:28:43 +0300

    Adding tests

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 import unittest
+import mock
 
 from mopidy_alarmclock import Extension
 
@@ -14,11 +15,33 @@ class ExtensionTest(unittest.TestCase):
 
         self.assertIn('[alarmclock]', config)
         self.assertIn('enabled = true', config)
-        # WARNING! Default configuration must be also updated in README
+
+        # WARNING! Default configuration must be also updated in README.rst and ext.conf
         self.assertIn('def_time = 8:00', config)
         self.assertIn('def_playlist = ', config)
         self.assertIn('def_random = false', config)
         self.assertIn('def_volume = 100', config)
         self.assertIn('def_vol_inc_duration = 30', config)
 
-    # TODO Write more tests
+    def test_get_config_schema(self):
+        ext = Extension()
+
+        schema = ext.get_config_schema()
+
+        # WARNING! Configuration element types not tested
+        self.assertIn('def_time', schema)
+        self.assertIn('def_playlist', schema)
+        self.assertIn('def_random', schema)
+        self.assertIn('def_volume', schema)
+        self.assertIn('def_vol_inc_duration', schema)
+
+    def test_setup():
+        registry = mock.Mock()
+
+        ext = Extension()
+        ext.setup(registry)
+
+        registry.add.assert_called_once_with('http:app', {
+            'name': 'alarmclock',
+            'factory': registry.add.call_args[0][1]['factory'],
+        })
