449a745f7dda3f8db146fb22f6fd95031db5de66
Davis Mosenkovs <davikovs@gmail.com>
2015-05-03 17:07:23 +0300

    Added first test for http

---
+++ tests/test_http.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+
+import unittest
+
+import mock
+
+import mopidy_alarmclock.http
+
+class HttpTest(unittest.TestCase):
+
+    def test_SetAlarmRequestHandler(self):
+        config = mock.Mock()
+        core = mock.Mock()
+        alarm_manager = mock.Mock()
+        msg_store = mock.Mock()
+
+        handler = http.SetAlarmRequestHandler()
+        handler.initialize(config, core, alarm_manager, msg_store)
+        handler.get_argument = mock.Mock()
+        handler.get_argument.side_effect = lambda v: {'playlist': 'Playlist URI', 'time': '8:00', 'random': '1', 'volume': '81', 'incsec': '23'}[v]
+        handler.post()
+
+        self.assertEqual(alarm_manager.set_alarm.call_count, 1)
+        self.assertEqual(alarm_manager.set_alarm.call_args[0][1], core.playlists.lookup('Playlist URI').get())
+        self.assertEqual(alarm_manager.set_alarm.call_args[0][2], True)
+        self.assertEqual(alarm_manager.set_alarm.call_args[0][3], 81)
+        self.assertEqual(alarm_manager.set_alarm.call_args[0][4], 23)
+
+    # TODO Use Tornado unit testing
+    # TODO Write more (granular + comprehensive) tests
