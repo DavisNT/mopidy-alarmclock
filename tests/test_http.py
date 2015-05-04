42b84bc918577460a447b208e5a016b6feeb00f2
Davis Mosenkovs <davikovs@gmail.com>
2015-05-04 03:36:37 +0300

    http tests: implementing

--- tests/test_http.py
+++ tests/test_http.py
@@ -9,7 +9,8 @@ from mopidy_alarmclock import http
 
 class HttpTest(unittest.TestCase):
 
-    def test_SetAlarmRequestHandler(self):
+    @mock.patch('mopidy_alarmclock.http.datetime.datetime.now')
+    def test_SetAlarmRequestHandler(self, mock_now):
         config = mock.Mock()
         core = mock.Mock()
         alarm_manager = mock.Mock()
@@ -21,8 +22,11 @@ class HttpTest(unittest.TestCase):
             handler = http.SetAlarmRequestHandler()
 
         handler.initialize(config, core, alarm_manager, msg_store)
+        handler.redirect = mock.Mock()
         handler.get_argument = mock.Mock()
         handler.get_argument.side_effect = lambda v, d: {'playlist': 'Playlist URI', 'time': '8:00', 'random': '1', 'volume': '81', 'incsec': '23'}[v]
+        mock_now.side_effect = datetime.datetime(2015, 5, 3, 11, 23, 31, 811)
+
         handler.post()
 
         self.assertEqual(alarm_manager.set_alarm.call_count, 1)
